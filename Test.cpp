5d3ea327456668746e655117726fc1ae2fee2cfa
maxkrakauer <maxkrakauer91@gmail.com>
2020-04-16 15:42:49 +0300

    Add files via upload

--- Test.cpp
+++ Test.cpp
@@ -20,11 +20,29 @@ using namespace std;
 
 TEST_CASE("Family of Jim") {
     family::Tree t("jim");
-    CHECK(t.relation("sam") == string("sam"));
+    CHECK(t.relation("jim") == string("me"));
     CHECK_THROWS(t.find("father"));
     t.addFather("jim","dan");
     t.addFather("dan","tim");
+    t.addMother("tim","dana");
+    CHECK(t.relation("dan") == string("father"));
+    CHECK(t.relation("tim") == string("grandfather"));
+    CHECK(t.relation("dana") == string("great-grandmother"));
+    CHECK_THROWS(t.relation("netanel"));
 
+}
+
+TEST_CASE("Family of robert"){
+    family::Tree t("robert");
+    t.addFather("robert","tommy");
+    t.remove("tommy");
+    CHECK(t.relation("tommy") == string("unrelated"));
+    t.addFather("robert","joe");
+    t.addFather("robert","thomas");
+    t.addMother("robert","tommy");
+    CHECK(t.find("mother") == string("tommy"));
+    CHECK(t.relation("joe") == string("unrelated"));
+    CHECK(t.find("father") == string("thomas"));
 }
 
 /* Add more test cases here */
